220e00885b15a142f0eb31cf4a6dfdb7e1ae6e50
Nicolas Esquilache <nicolas.esquilache@hotmail.com>
2023-09-19 08:57:58 -0300

    Cambiando de pc

--- src/components/CartWidget/CartWidget.jsx
+++ src/components/CartWidget/CartWidget.jsx
@@ -4,7 +4,7 @@ const CartWidget= () =>{
         <button className="btn btn-outline-primary position-relative">
         <i className="bi bi-cart"></i>
         <span className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger"> 
-        7
+        5
         <span className="visually-hidden">unread messeges</span>
         </span>
         </button>
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { initializeApp } from "firebase/app";
 import { BrowserRouter } from 'react-router-dom';
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
 import App from './App.jsx';
 import './index.css';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD5j0u8aks0FnzhoxUPBt6sfkRzYer39Ds",
+  authDomain: "coder-react-bd40e.firebaseapp.com",
+  projectId: "coder-react-bd40e",
+  storageBucket: "coder-react-bd40e.appspot.com",
+  messagingSenderId: "946658783268",
+  appId: "1:946658783268:web:ff9e5b3fa55822458368b1"
+};
+
+initializeApp(firebaseConfig);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
